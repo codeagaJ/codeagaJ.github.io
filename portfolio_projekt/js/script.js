dc6f36f7e8878d1af8ee6f36ba20fbb1df72a6a3
Agnieszka Jakubowska <jakubowska.agap@gmail.com>
2017-10-09 10:23:56 +0200

    dodałam przycisk w sekcji about

--- portfolio_projekt/js/script.js
+++ portfolio_projekt/js/script.js
@@ -1,35 +1,8 @@
 
-//$(document).ready(function() {
-//    
-//});
-//
-//$(window).scroll(function() {
-//    navAddClass();
-//});
-//
-//function navAddClass() {
-//    var navHeight = $('#main-nav').height();
-//    console.log(navHeight);
-//    
-//    var windoSrollPosition =$(window).scrollTop();
-//    console.log(windoSrollPosition);
-//    if(windoSrollPosition >= navHeight) {
-//        $('#main-nav').addClass('bg');
-//    }else {
-//        $('#main-nav').removeClass('bg');
-//    }
-//};
-//
 
 
-//$(document).ready(function() {
-//   $('#main-nav').on({
-//        'mouseover': function(){
-//            $(this).addClass('bg');
-//        },
-//        'mouseleave': function() {
-//            $(this).removeClass('bg');
-//        }
-//        
-//    });
-//});
+$(document).ready(function() {
+   $('#btn-about').click(function(){
+     $('.show p').toggle(2000);  
+   });
+});
