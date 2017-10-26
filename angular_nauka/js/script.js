be370b604e11d3add233473eeb089b9a24da070d
Agnieszka Jakubowska <jakubowska.agap@gmail.com>
2017-10-26 10:56:38 +0200

    inherit scope

--- angular_nauka/js/script.js
+++ angular_nauka/js/script.js
@@ -2,7 +2,7 @@ angular.module('app', [ ]);
 
 angular.module('app').controller('mainCtrl', function($scope) {
   
-    $scope.user = {
+    $scope.user1 = {
     name: 'Agnieszka ',
     address: {
       street: 'Wrocławska',
@@ -15,17 +15,57 @@ angular.module('app').controller('mainCtrl', function($scope) {
         'Tomek',
         'Marta'
       ]    
-  }   
+  }
+    
+    
+    $scope.user2 = {
+    name: 'Agnieszka ',
+    address: {
+      street: 'Wrocławska',
+    city: 'Warszawa',
+    country: 'Polska',
+    },
+      
+    friends:  [
+        'Dorota',
+        'Tomek',
+        'Marta'
+      ]    
+  }
 });
 
 angular.module('app').directive('userInfoCard', function() {
   return {
     templateUrl: "userInfoCard.html",
     restrict: "E",
+    scope: {
+      user: '='  
+    },
     controller: function($scope) {
+        $scope.collapsed = false;
         $scope.knightMe = function(user) {
         user.rank= "Dane";
       }
+      $scope.collapse = function() {
+          $scope.collapsed = !$scope.collapsed;
+      } 
     }
   }
 });
+
+angular.module('app').directive('address', function() {
+    return {
+        restrict: 'E',
+        scope: true,
+        templateUrl: 'address.html',
+        controller: function($scope) {
+        $scope.collapsed = false;
+        $scope.collapseAddress= function() {
+            $scope.collapsed = true;
+        }
+        $scope.expandAddress = function() {
+            $scope.collapsed = false;
+        }
+        }
+    }
+})
